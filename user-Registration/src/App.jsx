58dd54b211dd72dc2a2e514518244f8f30c7b26f
vanshrajput01 <rajputvansh009@gmail.com>
2023-11-08 13:30:15 +0530

    final Commit

--- user-Registration/src/App.jsx
+++ user-Registration/src/App.jsx
@@ -12,12 +12,14 @@ function App() {
   return (
     <div className="main-container">
       <div className="form-container">
+        <img className="logo" src="https://freepngimg.com/save/69662-instagram-media-brand-social-logo-photography/1200x627" alt="Logo-image"></img>
         <input hidden = {isLogin} type = "text" placeholder="Mobile Number or Email"></input>
         <input hidden = {isLogin} type = "text" placeholder="Full Name"></input>
         <input type = "text" placeholder="Phone Number,email or username"></input>
         <input type = "password" placeholder="Password"></input>
+        <br></br>
         <button>{isLogin ? "Sign in" : "Sign up"}</button>
-        <p>{isLogin ? "Don't Have an account" : "have an account "} <span onClick={signup}>{isLogin ? "Sign up" : "Log in"}</span></p>
+        <p>{isLogin ? "Don't Have an account ? " : "have an account "} <span onClick={signup}>{isLogin ? "Sign up" : "Log in"}</span></p>
 
 
 
