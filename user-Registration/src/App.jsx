ebb6adc6256893b371a942c98520a5cc8e8c285a
vanshrajput01 <rajputvansh009@gmail.com>
2023-11-08 13:08:09 +0530

    Second Commit "Some update on App"

--- user-Registration/src/App.jsx
+++ user-Registration/src/App.jsx
@@ -1,14 +1,35 @@
-
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [isLogin,setisLogin] = useState(true)
+
+  const signup = () =>{
+    setisLogin(!isLogin)
+  }
 
 
   return (
-    <>
-    <h1>User Registration</h1>
+    <div className="main-container">
+      <div className="form-container">
+        <input hidden = {isLogin} type = "text" placeholder="Mobile Number or Email"></input>
+        <input hidden = {isLogin} type = "text" placeholder="Full Name"></input>
+        <input type = "text" placeholder="Phone Number,email or username"></input>
+        <input type = "password" placeholder="Password"></input>
+        <button>{isLogin ? "Sign in" : "Sign up"}</button>
+        <p>{isLogin ? "Don't Have an account" : "have an account "} <span onClick={signup}>{isLogin ? "Sign up" : "Log in"}</span></p>
 
 
-    </>
+
+
+
+      </div>
+
+    
+    
+
+
+    </div>
   )
 }
 
